e868e343d66c1b933003aec1a697765746b831ea
Christian Colglazier <aquamorphcomputing@gmail.com>
2016-01-28 20:01:50 -0500

    Added two joysticks

--- src/org/usfirst/frc/team2642/robot/OI.java
+++ src/org/usfirst/frc/team2642/robot/OI.java
@@ -1,38 +1,32 @@
 package org.usfirst.frc.team2642.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+
 import org.usfirst.frc.team2642.robot.commands.ExampleCommand;
 
 /**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
+ * OI handles input from the user via joysticks and other controllers
+ * 
+ * @author Christian Colglazier
+ *
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+	public Joystick joystick1;
+	public Joystick joystick2;
+	
+	public OI() {
+		//Initialized joysticks to ports
+		joystick1 = new Joystick(0);
+		joystick2 = new Joystick(1);
+	}
+	
+	public Joystick getJoystick1() {
+		return joystick1;
+	}
+	
+	public Joystick getJoystick2() {
+		return joystick2;
+	}
 }
 
