8d812e034a97a0a039da7bb06067031b54a458c2
Christian Colglazier <aquamorphcomputing@gmail.com>
2016-02-01 18:54:57 -0500

    Added drive

--- src/org/usfirst/frc/team2642/robot/Robot.java
+++ src/org/usfirst/frc/team2642/robot/Robot.java
@@ -5,8 +5,12 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import org.usfirst.frc.team2642.robot.commands.Drive;
 import org.usfirst.frc.team2642.robot.commands.ExampleCommand;
+import org.usfirst.frc.team2642.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team2642.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -20,9 +24,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static DriveTrain driveTrain;
 	public static OI oi;
 
     Command autonomousCommand;
+    Command drive;
     SendableChooser chooser;
 
     /**
@@ -31,6 +37,7 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
+        driveTrain = new DriveTrain();
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
 //        chooser.addObject("My Auto", new MyAutoCommand());
